e464d853399f2429f37653a9ad39951c20a4eb68
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2023-01-03 10:15:01 -0800

    lowest common ancestor

---
+++ Level1/LCABinaryTreee.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+
+# My solution, beats 36% of solutions, implements recursion
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
+
+# Accepted answer, iterative & utilizies built-in "min & max" functions
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
