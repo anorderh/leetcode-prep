d097865c53e9baa8d1e8bab9bd63945a6c89a7fc
Anthony NorderhendqCDcd <anthony@norderhaug.org>
2023-01-27 08:31:28 -0800

    iterative backtracking

---
+++ Algorithm-I/Backtracking/ReverseLinkedList.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/reverse-linked-list/description/?envType=study-plan&id=algorithm-i
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None 
+        # Uses curr to relink list
+        # Uses head to identify when list is iterated
+
+        while head:
+            curr = head         # track head
+            head = head.next    # progress head
+
+            curr.next = preev   # curr.next is previous
+            prev = curr         # assign prev w/ curr
+        
+        return start
+        
